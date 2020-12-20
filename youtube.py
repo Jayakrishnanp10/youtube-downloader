ff15d4a7e80d9c64007739d335b20888b32155d3
jayakrishnan p <pjayakrishnan111@gmail.com>
2020-12-20 15:51:21 +0530

    ver1.02

--- youtube.py
+++ youtube.py
@@ -10,44 +10,61 @@ class youtube(Frame):
         self.master.title("YOUTUBE DOWNLOADER")
         self.master.geometry("500x200")
         self.master.configure(bg="#e85348")
-        self.p1 = PhotoImage(file ="C:/Users/pjaya/Documents/appu/python/youtube/yt.png")
+        self.p1 = PhotoImage(file ="C:/Users/pjaya/Documents/appu/python/youtube-downloader/yt.png")
         self.master.iconphoto(False, self.p1)
         self.configure(bg="#e85348")
         self.style=ttk.Style()
         self.style.theme_use("clam")
         self.grid()
+
         self.label1=Label(self,text="YOUTUBE DOWNLOADER")
         self.label1.grid(row=0,column=1,columnspan=3)
         self.label1.configure(bg="#e85348",fg='white',font='Helvetica 18 bold')
+
         self.label2=Label(self,text="Url:",font='Helvetica 8 bold')
         self.label2.grid(row=1,column=0,columnspan=1)
         self.label2.configure(bg="#e85348",fg='white')
+
         self.url=StringVar()
         self.urlentry=ttk.Entry(self,textvariable=self.url,width=50)
         self.urlentry.grid(row=1,column=1,columnspan=5)
+
         self.label3=Label(self,text="Location:",font='Helvetica 8 bold')
         self.label3.grid(row=2,column=0,columnspan=1)
         self.label3.configure(bg="#e85348",fg='white')
+
         self.loc=StringVar()
         self.locentry=ttk.Entry(self,textvariable=self.loc,width=50)
         self.locentry.grid(row=2,column=1,columnspan=5)
+        
         self.locb=ttk.Button(self,text="select folder",command=self.folselect)
         self.locb.grid(row=2,column=6,columnspan=3)
+        
         self.label3=Label(self,text="Resolution:",font='Helvetica 8 bold')
         self.label3.grid(row=3,column=0,columnspan=1)
         self.label3.configure(bg="#e85348",fg='white')
+        
         self.reso=StringVar()
         self.resselect=ttk.Combobox(self,textvariable=self.reso)
         self.resselect['values']=('144p','240p','360p','480p','720p','1080p','1440p','2160p')
         self.resselect.grid(row=3,column=1,columnspan=1)
         self.resselect.current(4)
+        
         self.submit=ttk.Button(self,text="download",command=self.ydownload)
         self.submit.grid(row=3,column=3,columnspan=3)
+
+        self.suc=StringVar()
+        self.suc.set(" ")
+        self.label4=Label(self,textvariable=self.suc,font='Helvetica 7 bold')
+        self.label4.grid(row=4,column=0,columnspan=5)
+        self.label4.configure(bg="#e85348",fg='white')
         
         
         
     def ydownload(self):
         YouTube(self.url.get()).streams.get_by_resolution(self.reso.get()).download(self.loc.get())
+        self.suc.set(self.url.get()+" "+self.reso.get()+" DOWNLOAD COMPLETED")
+
 
     
     def folselect(self):
